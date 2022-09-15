8427769e93c6b46afb2fb61c506334191eb124bd
rain.w <yu3.wang@sunyard.com>
2022-09-15 10:06:57 +0800

    init

---
+++ src/main/java/com/nanjolono/hikarimq/HikariMqApplication.java
@@ -0,0 +1,13 @@
+package com.nanjolono.hikarimq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikariMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( HikariMqApplication.class , args );
+    }
+
+}
---
+++ src/test/java/com/nanjolono/hikarimq/HikariMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanjolono.hikarimq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikariMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
