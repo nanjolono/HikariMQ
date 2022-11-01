8bd3b0643b44cfbedf9b9e92386c507b0ffc7c3a
nanjolono <nanjolono@outlook.com>
2022-11-01 20:18:37 +0800

    dev c

--- src/main/java/com/nanjolono/hikarimq/HikariMqApplication.java
+++ src/main/java/com/nanjolono/hikarimq/HikariMqApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HikariMqApplication {
 
     public static void main(String[] args) {
+	//C功能开发
         SpringApplication.run ( HikariMqApplication.class , args );
     }
 
